08dfcd30309e4d4802ab5199960eed3fe0a388e2
divyanjalimadhupada <137045587+divyanjalimadhupada@users.noreply.github.com>
2023-06-23 13:48:54 +0530

    removing duplicate categories without printing products

---
+++ objectsgithub/src/com/codebrains/training/product/modified/removeDuplicateCategories.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codebrains.training.product.modified;
+
+import com.codebrains.training.product.dto.Product;
+
+/**
+ *
+ * @author hp
+ */
+public class removeDuplicateCategories {
+
+    public static void main(String[] args) {
+
+        Product array[] = new Product[5];
+
+        Product prod1 = new Product(1, "biscuit", "edibles", 30, 2);
+        array[0] = prod1;
+        Product prod2 = new Product(2, "toothpaste", "household", 50, 2);
+        array[1] = prod2;
+        Product prod3 = new Product(3, "doraemon", "toys", 150, 2);
+        array[2] = prod3;
+        Product prod4 = new Product(4, "shinchan", "toys", 150, 2);
+        array[3] = prod4;
+        Product prod5 = new Product(5, "doormats", "household", 150, 2);
+        array[4] = prod5;
+
+        String category[] = new String[5];
+        for (int i = 0; i < array.length; i++) {
+            Product prod = array[i];
+            category[i] = prod.category;
+        }
+
+        int isthere ,k=0;
+        String uniqueCategory[] = new String[5];
+        for (int i = 0; i < category.length; i++) {
+            isthere = 0;
+            for (int j = 0; j < k; j++) {
+                if (category[i].equalsIgnoreCase(uniqueCategory[j])) {
+                    isthere = 1;
+                    break;
+                }}
+
+                if (isthere == 0) {
+                    uniqueCategory[k] = category[i];
+                    k++;
+
+                }
+            }
+                System.out.println("total no of categories");
+                for (int i = 0; i < k; i++) {
+                    System.out.println(uniqueCategory[i]);
+
+                }
+            }
+
+        }
+
+    
+
